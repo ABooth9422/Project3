c7aab29378b2f4b2ec29b76e77d6d45254d8a3e2
Jester <ABooth9422@gmail.com>
2019-09-05 14:18:16 -0400

    did some styling

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home/index"
 import NoMatch from "./pages/NoMatch/index";
-import Navbar from "./components/Navbar";
+
 
 function App() {
   return (
     <Router>
       <div>
-        <Navbar />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route component={NoMatch} />
--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -4,7 +4,7 @@ import './style.css'
 
 function Navbar(props) {
     return (
-        <nav className="navbar navbar-dark fixed-top navbar-expand-lg myNavbar">
+        <nav className="navbar navbar-dark navbar-expand-lg myNavbar">
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
