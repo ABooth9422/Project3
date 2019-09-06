ddbb0aa078ea94892c2acb1d27d92f0361989449
Jarvis Maness <you@example.com>
2019-09-05 20:44:52 -0400

    api routes created

---
+++ client/src/utils/API.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export default {
+    //Find Gym
+    getGyms: function() {
+        return axios.get("/api/gyms");
+    },
+    
+//Create routines
+    createRoutines: function(routineData) {
+        return axios.get("/api/routines", routineData);
+    }
+};
