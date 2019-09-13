0ea2d6b7517e54924888316e430b7da3f40fe5b5
Louis Rollins <louisrollins01@gmail.com>
2019-09-12 21:08:58 -0400

    added favWorkout field to user model

--- client/src/pages/Forums/index.js
+++ client/src/pages/Forums/index.js
@@ -12,11 +12,7 @@ function Forum() {
     <Jumbotron/>
       <Container>
       <div className='container justify-content-center p-5'>    
-            <h1>Forum Page</h1>
-            <p>The Gym Sense forums are a place to ask questions, discuss routines, debate dietary practices, 
-              or share personal accomplishments to inspire others. Please be considerate and respectful to other
-              Gym Sense members. Moderators reserve the right to delete any content deemed unfit for the Gym Sense forum.
-            </p>            
+            <h1>Forum Page</h1>         
       </div>
       </Container>
     </Wrapper>
