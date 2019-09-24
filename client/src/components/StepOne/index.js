473169683453ea10b4edfa172b7ba87ae378363d
Louis Rollins <louisrollins01@gmail.com>
2019-09-24 19:14:58 -0400

    checkout boxes and number dropdown complete

--- client/src/components/StepOne/index.js
+++ client/src/components/StepOne/index.js
@@ -5,30 +5,60 @@ class StepOne extends Component {
   constructor () {
     super()
     this.state = { 
-      firstName: '', 
-      lastName: ''
+
     }
-    this.handleFirstNameChanged = this.handleFirstNameChanged.bind(this);
-    this.handleLastNameChanged = this.handleLastNameChanged.bind(this);
   }
 
-  handleFirstNameChanged (event) {
-    this.setState({firstName: event.target.value})
-  }
 
-  handleLastNameChanged (event) {
-    this.setState({lastName: event.target.value})
-  }
 
   render () {
     return (
       <div className="container">
         <div className="row">
-          <div className="col-12 test">
-            <h3>Test</h3>
+          <div className="col-12">
+            <div className="form-check form-check-inline">
+              <input className="form-check-input" type="checkbox" id="chest" value="chest" />
+            <label className="form-check-label" for="chest">Chest</label>
+            </div>
+          <div className="form-check form-check-inline">
+            <input className="form-check-input" type="checkbox" id="legs" value="legs" />
+            <label className="form-check-label" for="legs">Legs</label>
+          </div>
+          <div className="form-check form-check-inline">
+              <input className="form-check-input" type="checkbox" id="biceps" value="biceps" />
+            <label className="form-check-label" for="biceps">Biceps</label>
+            </div>
+          <div className="form-check form-check-inline">
+            <input className="form-check-input" type="checkbox" id="triceps" value="triceps" />
+           <label className="form-check-label" for="triceps">Triceps</label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input className="form-check-input" type="checkbox" id="shoulders" value="shoulders" />
+           <label className="form-check-label" for="shoulders">Shoulders</label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input className="form-check-input" type="checkbox" id="back" value="back" />
+           <label className="form-check-label" for="back">Back</label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input className="form-check-input" type="checkbox" id="cardio" value="cardio" />
+           <label className="form-check-label" for="cardio">Cardio</label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input className="form-check-input" type="checkbox" id="abs" value="abs" />
+           <label className="form-check-label" for="abs">Abs</label>
           </div>
         </div>
       </div>
+      <div className="row justify-content-center">
+        <div className="col-md-2">
+        <div class="form-group">
+          <label for="exampleFormControlInput1">Select the number of exercises you would like to complete.</label>
+          <input type="number" class="form-control" min="1" max="5" id="workoutNumber" placeholder="" />
+        </div>
+        </div>
+      </div>
+    </div>
     )
   }
 }
