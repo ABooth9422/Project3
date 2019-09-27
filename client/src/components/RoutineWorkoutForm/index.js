09062a9ac0a1553ac10df95501b780c369de65d7
Mark <mrbruce9866@gmail.com>
2019-09-27 19:17:47 -0400

    fixed duplicate workouts

--- client/src/components/RoutineWorkoutForm/index.js
+++ client/src/components/RoutineWorkoutForm/index.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react'
 import Button from '../Button'
-import "./style.css"
+import './style.css'
 
 class RoutineWorkoutForm extends Component {
- 
-
   goBack = (event) => {
     event.preventDefault()
     this.props.prevStep()
@@ -12,70 +10,74 @@ class RoutineWorkoutForm extends Component {
 
   render () {
     const { values, handleChange, handleCheckboxChange } = this.props
-    const workouts = values.selectedWorkouts;
+    const workouts = values.selectedWorkouts
 
     return (
       <>
-      <div className="tableWrap mx-5 p-5">
-        <table style={{"boxShadow":"black 3px 3px 3px"}}className="table table-dark rounded table-striped">
-  <thead>
-    <tr>
-      <th scope="col-5 underline">Workout</th>
-      <th scope="col-3 underline">Reps/Carbs</th>
-      <th scope="col-3 underline">Sets</th>
-      <th scope="col-1 underline"></th>
-    </tr>
-  </thead>
-  <tbody>
-    
-            {workouts &&
-              workouts.map((w) => {
-                return (
-                  <tr>
-                    <td>
-                      <i style={{ color: '#167F8F' }} onClick={()=>this.props.infoHandle(w.excercise.videoLink )} className='fa fa-info-circle mx-2' />
-                      {w.excercise.workout}
-                      <span style={{ color: '#D52323', marginLeft: '10px' }}>({w.excercise.muscleGroup})</span>
-                    </td>
-                    <td>
-                      <input
-                        type='number'
-                        className='form-control'
-                        id={`${w.excercise.workout}Reps`}
-                        min='5'
-                        max='100'
-                        step='5'
-                        value={w.excercise.muscleGroup === "Cardio" ? w.cals : w.reps}
-                        style={{ width: '50%', minWidth: '4rem', margin: '0 auto' }}
-                      />
-                    </td>
-                    <td>
-                      {w.excercise.muscleGroup !== 'Cardio' && (
+        <div className='tableWrap mx-5 p-5'>
+          <table style={{ boxShadow: 'black 3px 3px 3px' }} className='table table-dark rounded table-striped'>
+            <thead>
+              <tr>
+                <th scope='col-5 underline'>Workout</th>
+                <th scope='col-3 underline'>Reps/Carbs</th>
+                <th scope='col-3 underline'>Sets</th>
+                <th scope='col-1 underline' />
+              </tr>
+            </thead>
+            <tbody>
+              {workouts &&
+                workouts.map((w, index) => {
+                  return (
+                    <tr>
+                      <td>
+                        <i
+                          style={{ color: '#167F8F' }}
+                          onClick={() => this.props.infoHandle(w.excercise.videoLink)}
+                          className='fa fa-info-circle mx-2'
+                        />
+                        {w.excercise.workout}
+                        <span style={{ color: '#D52323', marginLeft: '10px' }}>({w.excercise.muscleGroup})</span>
+                      </td>
+                      <td>
                         <input
                           type='number'
                           className='form-control'
-                          id={`${w.excercise.workout}Sets`}
-                          min='1'
-                          max='10'
-                          val={w.sets}
+                          id={`${w.excercise.workout}Reps`}
+                          min='5'
+                          max='100'
+                          step='5'
+                          value={w.excercise.muscleGroup === 'Cardio' ? w.cals : w.reps}
                           style={{ width: '50%', minWidth: '4rem', margin: '0 auto' }}
                         />
-                      )}
-                    </td>
-                    <td>
-                    <Button className='btn btn-lg btn-danger'><i class="fa fa-trash"></i></Button>
-                    </td>
-                  </tr>
-                )
-              })}
-  </tbody>
-</table>
-</div>
-          <div className='col-12'>
-            <Button clickHandle={this.goBack}>Go Back</Button>
-            <Button clickHandle={this.props.submit}>Finished</Button>
-          </div>
-        
+                      </td>
+                      <td>
+                        {w.excercise.muscleGroup !== 'Cardio' && (
+                          <input
+                            type='number'
+                            className='form-control'
+                            id={`${w.excercise.workout}Sets`}
+                            min='1'
+                            max='10'
+                            val={w.sets}
+                            style={{ width: '50%', minWidth: '4rem', margin: '0 auto' }}
+                          />
+                        )}
+                      </td>
+                      <td>
+                        <Button className='btn btn-lg btn-danger'>
+                          <i className='fa fa-trash' />
+                        </Button>
+                      </td>
+                    </tr>
+                  )
+                })}
+            </tbody>
+          </table>
+        </div>
+        <div className='col-12'>
+          <Button clickHandle={this.goBack}>Go Back</Button>
+          <Button clickHandle={this.props.submit}>Finished</Button>
+        </div>
       </>
     )
   }
--- client/src/pages/Gyms/index.js
+++ client/src/pages/Gyms/index.js
@@ -1,119 +1,117 @@
-import React,{Component} from "react";
-import Container from "../../components/Container";
-import Jumbotron from "../../components/Jumbotron";
-import Wrapper from "../../components/Wrapper";
-import GymCard from "../../components/GymCard";
-import Form from '../../components/Form';
-import TextInput from '../../components/TextInput';
-import Button from '../../components/Button';
-import {GoogleMap, withScriptjs, withGoogleMap, Marker} from 'react-google-maps';
-import "./style.css";
-import API from "../../utils/API";
+import React, { Component } from 'react'
+import Container from '../../components/Container'
+import Jumbotron from '../../components/Jumbotron'
+import Wrapper from '../../components/Wrapper'
+import GymCard from '../../components/GymCard'
+import Form from '../../components/Form'
+import TextInput from '../../components/TextInput'
+import Button from '../../components/Button'
+import { GoogleMap, withScriptjs, withGoogleMap, Marker } from 'react-google-maps'
+import './style.css'
+import API from '../../utils/API'
 
-class Gyms extends Component{
-  constructor(){
+class Gyms extends Component {
+  constructor () {
     super()
 
-  this.state={
-    WrappedMap: null,
-    selectedGym: null,
-    query: '',
-    center:{lat: 35.779591, lng: -78.638176},
-    gyms: [],
-    favoriteGyms: []
-
+    this.state = {
+      WrappedMap: null,
+      selectedGym: null,
+      query: '',
+      center: { lat: 35.779591, lng: -78.638176 },
+      gyms: [],
+      favoriteGyms: []
+    }
   }
-}
- 
-  componentDidMount(){
-    if(!this.state.WrappedMap){
-      this.initMap();
+
+  componentDidMount () {
+    if (!this.state.WrappedMap) {
+      this.initMap()
     }
 
-    this.updateFavorites();
-   
+    this.updateFavorites()
   }
   getLocation = () => {
-    navigator.geolocation.getCurrentPosition(this.showPosition);
+    navigator.geolocation.getCurrentPosition(this.showPosition)
   }
 
-  showPosition = (position)=>{
-    this.setState({location: position})
+  showPosition = (position) => {
+    this.setState({ location: position })
     console.log(this.state.location)
   }
-  
 
-  useLocation=()=>{
-    if(!this.state.disabled){
-      this.setState({disabled:true})
-    }else{
-      this.setState({disabled:false})
+  useLocation = () => {
+    if (!this.state.disabled) {
+      this.setState({ disabled: true })
+    } else {
+      this.setState({ disabled: false })
     }
-      
   }
 
   formSubmit = () => {
-    if(this.state.query){
+    if (this.state.query) {
       console.log(this.state.query)
-      API.getGymsWithoutLocation(this.state.query).then(res =>{
-        this.setState({center: res.data.center, gyms: res.data.gyms, WrappedMap: withScriptjs(withGoogleMap(this.Map)), selectedGym: null});
-        console.log(this.state);
-      }).catch(err => {
-        console.log(err.response);
-      })
+      API.getGymsWithoutLocation(this.state.query)
+        .then((res) => {
+          this.setState({
+            center: res.data.center,
+            gyms: res.data.gyms,
+            WrappedMap: withScriptjs(withGoogleMap(this.Map)),
+            selectedGym: null
+          })
+          console.log(this.state)
+        })
+        .catch((err) => {
+          console.log(err.response)
+        })
+    }
   }
-}
 
   inputChange = (event) => {
-    
     this.setState({
       query: event.target.value,
       renderMap: false
-    });
+    })
   }
 
-  selectGym(gym){
-    console.log(gym);
-    this.setState({selectedGym: gym})
-    if('photos' in gym){
-      API.getGymPhoto(gym.photos[0].photo_reference).then(response => {
-        
-      })
+  selectGym (gym) {
+    console.log(gym)
+    this.setState({ selectedGym: gym })
+    if ('photos' in gym) {
+      API.getGymPhoto(gym.photos[0].photo_reference).then((response) => {})
     }
-    
   }
 
-  Map =() =>{
+  Map = () => {
     return (
       <GoogleMap defaultZoom={10} defaultCenter={this.state.center}>
-        {this.state.gyms.map(gym =>(
-          <Marker key={gym.id} 
-          position={gym.geometry.location} 
-          onClick={()=>{
-            this.selectGym(gym);
-          }}
-          icon={
-            {
+        {this.state.gyms.map((gym) => (
+          <Marker
+            key={gym.id}
+            position={gym.geometry.location}
+            onClick={() => {
+              this.selectGym(gym)
+            }}
+            icon={{
               url: require('./images/marker.png'),
-              scaledSize: new window.google.maps.Size(50,50)
-            }
-          }
+              scaledSize: new window.google.maps.Size(50, 50)
+            }}
           />
         ))}
       </GoogleMap>
     )
   }
-  initMap =()=>{
-    this.setState({WrappedMap: withScriptjs(withGoogleMap(this.Map))})
+  initMap = () => {
+    this.setState({ WrappedMap: withScriptjs(withGoogleMap(this.Map)) })
   }
 
-  toggleFavoriteGym = () =>{
-    if(this.state.favoriteGyms.includes(this.state.selectedGym.id)){
-      API.removeFavoriteGym(this.state.selectedGym.id, this.props.profile.id).then(response=>{
-        this.updateFavorites();
+  toggleFavoriteGym = () => {
+    if (this.state.favoriteGyms.includes(this.state.selectedGym.id)) {
+      API.removeFavoriteGym(this.state.selectedGym.id, this.props.profile.id).then((response) => {
+        this.updateFavorites()
       })
-    }else{
-      const sGym = this.state.selectedGym;
+    } else {
+      const sGym = this.state.selectedGym
       const gymObj = {
         gymId: sGym.id,
         name: sGym.name,
@@ -121,92 +119,96 @@ class Gyms extends Component{
         rating: sGym.rating,
         img: 'photos' in sGym ? sGym.photos[0].photo_reference : 'https://hondafsj.com/dist/img/nophoto.jpg',
         UserId: this.props.profile.id
-
       }
 
-      console.log(gymObj);
+      console.log(gymObj)
 
-      API.addFavoriteGym(gymObj).then(response=>{
-        this.updateFavorites();
+      API.addFavoriteGym(gymObj).then((response) => {
+        this.updateFavorites()
       })
-     
     }
-
-    
   }
 
-  updateFavorites(){
-    this.props.getProfile(res =>{
-      this.setState({favoriteGyms: res.favGyms.map(gym=>gym.gymId)});
-    });
+  updateFavorites () {
+    this.props.getProfile((res) => {
+      this.setState({ favoriteGyms: res.favGyms.map((gym) => gym.gymId) })
+    })
   }
 
- 
+  render () {
+    let WrappedMap = this.state.WrappedMap || withScriptjs(withGoogleMap(this.Map))
 
-  render(){
-    let WrappedMap = this.state.WrappedMap || withScriptjs(withGoogleMap(this.Map));
-    
-  
-  return (
-    <>
-    <Wrapper>
-    <Jumbotron/>
-      <Container>
-          <header className="my-1 p-5rounded">
-          <div className="gym__bg p-5 "></div>
-          <h1 id="gymHead" className="display-1 rounded mt-5 text-black">Find your gym!</h1>
-          </header>
-          <div style={{"boxShadow":"black 3px 3px 3px"}}className="contain rounded mx-5 p-5">
-          <div className="row d-flex justify-content-center text-center">
-          </div>
-          <div className="row d-flex my-3  justify-content-center text-center">
-          <h3>Our desire is to help you find a gym in your area and be a resource for starting your workout routines. Enter your address or zip code below to find a gym to FIT you needs!
-          </h3>
-          </div>
-          
-        <div className='row justify-content-center'>
-           
-            <Form>
-              <TextInput id='gymSearch' placeholder={"Enter Address"} changeHandle={this.inputChange}>{this.state.query}</TextInput>
-              <Button type='button' bootType='danger' clickHandle={this.formSubmit} >GO</Button>
-              </Form>
-           
-        </div>
-        </div>
-        <div className='row text-center mx-2 my-5 justify-content-center'>
-            <div className='col-12 col-md-6 my-3 rounded mapContainer'>
-            <WrappedMap 
-              googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
-              loadingElement={<div style={{height: '100%'}}></div>}
-              containerElement={<div style={{height: '100%',  maxHeight: '500px'}}></div>}
-              mapElement={<div style={{height: '100%'}}></div>}
-            />
+    return (
+      <>
+        <Wrapper>
+          <Jumbotron />
+          <Container>
+            <header className='my-1 p-5rounded'>
+              <div className='gym__bg p-5 ' />
+              <h1 id='gymHead' className='display-1 rounded mt-5 text-black'>
+                Find your gym!
+              </h1>
+            </header>
+            <div style={{ boxShadow: 'black 3px 3px 3px' }} className='contain rounded mx-5 p-5'>
+              <div className='row d-flex justify-content-center text-center' />
+              <div className='row d-flex my-3  justify-content-center text-center'>
+                <h3>
+                  Our desire is to help you find a gym in your area and be a resource for starting your workout
+                  routines. Enter your address or zip code below to find a gym to FIT you needs!
+                </h3>
+              </div>
+
+              <div className='row justify-content-center'>
+                <Form>
+                  <TextInput id='gymSearch' placeholder={'Enter Address'} changeHandle={this.inputChange}>
+                    {this.state.query}
+                  </TextInput>
+                  <Button type='button' clickHandle={this.formSubmit}>
+                    GO
+                  </Button>
+                </Form>
+              </div>
             </div>
-            {this.state.selectedGym?
-            <div className='col-12 col-md-6 my-3'>
-            
-            {this.state.selectedGym && (
-                
-                <GymCard 
-                id= {this.state.selectedGym.id}
-                  name= {this.state.selectedGym.name}
-                  favClick= {this.toggleFavoriteGym}
-                  address= {this.state.selectedGym.vicinity }
-                  rating= {this.state.selectedGym.rating}
-                  favorited={this.state.favoriteGyms.includes(this.state.selectedGym.id)}
-                  img= {'photos' in this.state.selectedGym ? `https://maps.googleapis.com/maps/api/place/photo?maxheight=300&photoreference=${this.state.selectedGym.photos[0].photo_reference}&key=${process.env.REACT_APP_GOOGLE_KEY}`: 'https://hondafsj.com/dist/img/nophoto.jpg'}
-                  
-                  />
+            <div className='row text-center mx-2 my-5 justify-content-center'>
+              <div className='col-12 col-md-6 my-3 rounded mapContainer'>
+                <WrappedMap
+                  googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${
+                    process.env.REACT_APP_GOOGLE_KEY
+                  }`}
+                  loadingElement={<div style={{ height: '100%' }} />}
+                  containerElement={<div style={{ height: '100%', maxHeight: '500px' }} />}
+                  mapElement={<div style={{ height: '100%' }} />}
+                />
+              </div>
+              {this.state.selectedGym ? (
+                <div className='col-12 col-md-6 my-3'>
+                  {this.state.selectedGym && (
+                    <GymCard
+                      id={this.state.selectedGym.id}
+                      name={this.state.selectedGym.name}
+                      favClick={this.toggleFavoriteGym}
+                      address={this.state.selectedGym.vicinity}
+                      rating={this.state.selectedGym.rating}
+                      favorited={this.state.favoriteGyms.includes(this.state.selectedGym.id)}
+                      img={
+                        'photos' in this.state.selectedGym
+                          ? `https://maps.googleapis.com/maps/api/place/photo?maxheight=300&photoreference=${
+                            this.state.selectedGym.photos[0].photo_reference
+                          }&key=${process.env.REACT_APP_GOOGLE_KEY}`
+                          : 'https://hondafsj.com/dist/img/nophoto.jpg'
+                      }
+                    />
+                  )}
+                </div>
+              ) : (
+                <></>
               )}
-            
             </div>
-            :<></>}
-        </div>
-      </Container>
-    </Wrapper>
-    </>
-  );
+          </Container>
+        </Wrapper>
+      </>
+    )
   }
 }
 
-export default Gyms;
+export default Gyms
